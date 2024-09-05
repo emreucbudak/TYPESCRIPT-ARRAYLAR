31657c0266a26f4c7f0217d25e42bb684629886f
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-09-05 15:22:45 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+var array = ["Emre Üçbudak", "KTÜ", "WEB DEVELOPER"];
+console.log(array);
+var arr = [1, 3, 4, 5, 6, 7, "Emre Üçbudak"];
+var values = ["emreeqweqweqwe", "denemememmememem", 45, 89, 769];
+console.log(arr);
+console.log(values);
---
+++ app.ts
@@ -0,0 +1,6 @@
+let array:string[] = ["Emre Üçbudak","KTÜ","WEB DEVELOPER"]
+console.log(array)
+let arr= [1,3,4,5,6,7,"Emre Üçbudak"]
+let values: (string | number)[] = ["emreeqweqweqwe","denemememmememem",45,89,769]
+console.log(arr)
+console.log(values)
